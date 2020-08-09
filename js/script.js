8b2285ecc8d49bf3ab44e2c331b5f928abcff5f3
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2020-08-09 23:53:29 +0600

    modified:   index.html 	modified:   js/script.js 	modified:   practice.html

---
+++ js/script.js
@@ -0,0 +1,108 @@
+
+   /* var name;
+    name = prompt ("Name ?");
+    document.write(name);
+    document.write("<h2>" + name + "</h2>");
+    document.write(document.title + "<br>");
+    var xxx = document.getElementsByTagName('p1');
+    document.write(xxx + "</br>");
+    document.write(xxx.length);
+
+    function msg(){
+            var name;
+            name = prompt("What is you name ?");
+            document.write("<h1>" + name + "</h1>");
+        }
+    */
+   /*
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + "event";
+    }
+
+    function displayDate(){
+        document.getElementById("demo").innerHTML=Date();
+    }
+    
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + " event";
+    }
+
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + " event";
+    }
+
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + " event";
+    }
+
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + " event";
+    }
+
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + " event";
+    }
+
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + " 7event";
+    }
+
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + " 8event";
+    }
+
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + " 9event";
+    }
+
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + " 0event";
+    }
+    */
+   /*
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + " 1xEvent";
+    }
+    function displayDate(){
+        document.getElementById('demo').innerHTML = Date();
+    }
+
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + " 2xEvent";
+    }
+    function displayDate(){
+        document.getElementById('demo').innerHTML = Date();
+    }
+
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + " 3xEvent";
+    }
+    function displayDate(){
+        document.getElementById('demo').innerHTML = Date();
+    }
+    function message(msg){
+        document.getElementById('output').innerHTML = msg + " 4xEvent";
+    }
+    function message(msg,kkk){
+        document.getElementById('output').innerHTML = msg + " 5xEvent"
+    }
+
+*/
+/*
+    function test(){
+        var name;
+        name = prompt("Name");
+        document.write("<br>" + name);
+        document.write("<h1>" + name + "</h1>");
+    }
+*/
+    var z = "Yo";
+    test2(z);
+    console.log(z);
+    x = "Tata";
+    test2(x);
+    console.log(x);
+
+    function test2(name){
+        alert(name);
+    }
