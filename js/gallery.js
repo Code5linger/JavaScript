ef40587c2c3ace4ca35e6bbaa52b9306e033b4b6
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2020-08-16 04:07:04 +0600

    new file:   GalleryHomework.html 	new file:   js/gallery.js 	new file:   this-2nd-test.html 	new file:   this-2nd.html 	modified:   this-practice.html

---
+++ js/gallery.js
@@ -0,0 +1,97 @@
+/*
+function upDate(element){
+    document.getElementById('image').innerHTML = element.alt;
+            }
+       function unDo(){           
+       }
+*/
+/*
+function upDate(previewPic){
+    var src = previewPic.getAttribute("src");
+    document.getElementById('image').style.backgroundImage = "url('"+ src + "')";
+    
+        document.getElementById('image').innerHTML = previewPic.alt;
+    }
+    function unDo(){
+        document.getElementById('image').style.backgroundImage = "url('')";
+    
+        document.getElementById('image').innerHTML = 'Hover over an image below to display here';
+    }
+*/
+/*
+   function upDate(previewPic){
+    document.getElementById('image').style.backgroundImage = "url(" + previewPic.src + ")"; 
+     document.getElementById('image').innerHTML= previewPic.alt;
+ }
+ function unDo(){
+    document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+    document.getElementById('image').style.backgroundImage = "url('')";	
+ }
+*/
+/*
+function upDate(previewPic) {
+    document.getElementById('image').style.backgroundImage = "url(" + previewPic.src + ")";
+    document.getElementById('image').innerHTML = previewPic.alt;
+  }
+  
+  function unDo() {
+    document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+    document.getElementById('image').style.backgroundImage = "url('')";
+  }
+*/
+/*
+function upDate(previewPic){
+    var div = document.getElementById('image');
+    div.style.backgroundImage = "url('" + previewPic.src + "')";
+    div.innerHTML = previewPic.alt;
+      }
+  
+  function unDo(){
+  
+      var div = document.getElementById('image');
+    div.style.backgroundImage = "url('')";
+    div.innerHTML = "Hover over an image below to display here.";
+     
+      }
+*/
+/*
+function upDate(previewPic){
+     document.getElementById("image").style.backgroundImage = "url("+previewPic.src+")"
+
+     document.getElementById("image").innerHTML = previewPic.alt;
+       }
+   
+       function unDo(){
+        document.getElementById("image").style.backgroundImage="url(' ')";
+
+        document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+       }
+
+      function upDate(abc){
+        document.getElementById('image').innerHTML = abc.alt;
+        document.getElementById('image').style.backgroundImage = "url("+ abc.src + ")";
+     }
+
+     function unDo(){
+         x = document.getElementById('image');
+         x.innerHTML =" Hover over an image below to display here.";
+         x.style.backgroundImage = "url('')";
+     }
+*/
+/*
+function upDate(previewPic) {
+    var nuevoAlt = previewPic.alt;
+    var nuevaImagen = previewPic.src;
+    
+    document.getElementById('image').innerHTML = nuevoAlt;
+    document.getElementById('image').style.background = "url('" + nuevaImagen + "')";
+}
+
+function unDo() {
+    var Altinicial = "Hover over an image below to display here."
+    document.getElementById('image').innerHTML = Altinicial;
+    
+    document.getElementById('image').style.background = '#8e68ff';
+    document.getElementById('image').style.backgroundImage = "url('')";
+}
+*/
