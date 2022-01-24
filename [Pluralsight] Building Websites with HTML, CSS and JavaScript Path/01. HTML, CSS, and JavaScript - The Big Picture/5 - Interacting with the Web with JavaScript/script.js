56be0639c9e070de906ce405aa1135ffcf63be85
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-01-24 22:17:54 +0600

    What is your name

---
+++ [Pluralsight] Building Websites with HTML, CSS and JavaScript Path/01. HTML, CSS, and JavaScript - The Big Picture/5 - Interacting with the Web with JavaScript/script.js
@@ -0,0 +1,8 @@
+function Prompt() {
+  var txt;
+  var name = window.prompt("What is your name");
+  if (name != null || name != "") {
+    txt = "Hello " + name;
+  }
+  document.getElementById("name").innerHTML = txt;
+}
