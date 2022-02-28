3026a5de630e8af11c9024e973c725dd53bd85d7
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-03-01 05:25:09 +0600

    x = x + 1;

---
+++ [Pluralsight] JavaScript Core Language Path v2/04. Arrays and Collections 116'/script.js
@@ -0,0 +1,19 @@
+/*
+let price = 20.99,
+  texRate = 0.07;
+
+console.log(price * texRate);
+
+x = x + 1;
+*/
+/*
+let amount = 20;
+let x = 10;
+--x;
+console.log(amount);
+console.log(x);
+*/
+let name = "Copa";
+let text = `Hello ${name}`;
+
+console.log(text);
