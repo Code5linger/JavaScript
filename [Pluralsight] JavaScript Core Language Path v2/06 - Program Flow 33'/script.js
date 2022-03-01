8a1b553dc0198a58fc62112a1cafd614c28a13a7
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-03-01 10:03:13 +0600

    Control Flow

---
+++ [Pluralsight] JavaScript Core Language Path v2/06 - Program Flow 33'/script.js
@@ -0,0 +1,29 @@
+if (5 == 5) {
+  console.log("Yes");
+}
+
+if (5 > 10) {
+  console.log("No");
+}
+
+if (5 >= 5) {
+  console.log("Meh");
+}
+
+let state = "Fl";
+let tax = 7;
+
+if (state !== "Fl") {
+  tax = 0;
+}
+
+console.log(tax);
+
+if (1 === 2) {
+  c;
+}
+
+let price = 20;
+if (price > 15) {
+  console.log("discounted");
+}
