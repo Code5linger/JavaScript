196a80f1b8919a45ad10aed0aecad5f53741e3ac
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-03-02 10:46:25 +0600

    Array

---
+++ [Pluralsight] JavaScript Core Language Path v2/09 - Arrays 31'/script.js
@@ -0,0 +1,13 @@
+/*
+let values = [1, 2, 3];
+
+console.log(Array.isArray(values));
+*/
+/*
+let values = ["a", "b", "c"];
+values[0] = "XxX";
+console.log(values[0]);
+console.log(values[1]);
+console.log(values[2]);
+console.log(values[3]);
+*/
