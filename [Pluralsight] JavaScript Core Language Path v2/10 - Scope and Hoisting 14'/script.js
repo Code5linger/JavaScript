839e369d2986f665ed23a8bd9b1388a18da953e1
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-03-05 16:34:48 +0600

    Scope

---
+++ [Pluralsight] JavaScript Core Language Path v2/10 - Scope and Hoisting 14'/script.js
@@ -0,0 +1,21 @@
+let productId = 12345;
+
+function test() {
+  let customer = "Copa";
+  console.log(productId);
+  console.log(customer);
+}
+
+test();
+
+const app = {
+  id: 123,
+  user: "Double Copa",
+  order: 22,
+};
+
+function alpha() {
+  console.log(app.user);
+}
+
+alpha();
