8e2efa2bbc6c5bf75940f3a4dadf6e7163b1d082
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-03-16 21:46:05 +0600

    Var

--- [Pluralsight] JavaScript Core Language Path v2/03. Variables and Types 42'/script.js
+++ [Pluralsight] JavaScript Core Language Path v2/03. Variables and Types 42'/script.js
@@ -1,11 +1,22 @@
-let empty;
+/*let empty;
 
 console.log(empty);
-
+*/
 /*Changing Variable Value*/
-
+/*
 let price;
 
 price = 49.99;
 
 console.log(price);
+*/
+/*
+const price;
+
+console.log(price);
+*/
+/*
+console.log(price);
+
+var price = 99.99;
+*/
