bd581b91c65ddd9a874441a4748e89aca29c6b84
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-03-23 19:49:34 +0600

    Symbol()

--- [Pluralsight] JavaScript Core Language Path v2/08 - Objects and the DOM 36'/script.js
+++ [Pluralsight] JavaScript Core Language Path v2/08 - Objects and the DOM 36'/script.js
@@ -1,13 +1,18 @@
+let mySymbol = Symbol();
+
 let person = {
   name: "Jon",
   age: 32,
   job: true,
+  [mySymbol]: "Secret",
 };
 
-person.name = "Copa";
+//person.name = "Copa";
+person["name"] = "Samsu";
 
 console.log(person);
 console.log(person.name);
 console.log(person.age);
 console.log(person.job);
 console.log(person.country);
+console.log(person.mySymbol);
