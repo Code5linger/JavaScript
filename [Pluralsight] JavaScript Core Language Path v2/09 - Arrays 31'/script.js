1e92b7b3db7dff41c2878c2c32a8c80e47e3f4b9
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-03-26 18:34:45 +0600

    Access every Items from an initialized Array

--- [Pluralsight] JavaScript Core Language Path v2/09 - Arrays 31'/script.js
+++ [Pluralsight] JavaScript Core Language Path v2/09 - Arrays 31'/script.js
@@ -107,7 +107,13 @@ console.log(Array[0]);
 let values = Array.of("1", "2", "3");
 console.log(values[0]);
 */
-
+/*
 const values = ["a", "b", "c"];
 console.log(typeof values);
 console.log(Array.isArray(values));
+*/
+const values = ["a", "b", "c"];
+console.log(values[0]);
+console.log(values[1]);
+console.log(values[2]);
+console.log(values[3]);
