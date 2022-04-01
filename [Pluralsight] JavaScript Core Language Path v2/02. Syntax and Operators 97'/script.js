994f9af63bc257e1213440924704bf57095ef336
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-04-01 16:21:51 +0600

    W

--- [Pluralsight] JavaScript Core Language Path v2/02. Syntax and Operators 97'/script.js
+++ [Pluralsight] JavaScript Core Language Path v2/02. Syntax and Operators 97'/script.js
@@ -1,3 +1,4 @@
+/*
 function simpleSwitch() {
   let productId = "x";
 
@@ -13,3 +14,160 @@ function simpleSwitch() {
       break;
   }
 }
+*/
+//1
+/*
+function simpleSwitch() {
+  let id = 10;
+  switch (id) {
+    case 1:
+      console.log(1);
+      break;
+    case 2:
+      console.log(2);
+      break;
+    default:
+      console.log("69");
+      break;
+  }
+}
+*/
+//2
+/*
+function simpleSwitch() {
+  let id = 10;
+  switch (id) {
+    case 1:
+      console.log(1);
+      break;
+    case 2:
+      console.log(2);
+      break;
+    case 3:
+      console.log(3);
+      break;
+    default:
+      console.log(69);
+      break;
+  }
+}
+*/
+//3
+/*
+function simpleSwitch() {
+  let id = 30;
+  switch (id) {
+    case 1:
+      console.log(1);
+      break;
+    case 2:
+      console.log(2);
+      break;
+    case 3:
+      console.log(3);
+      break;
+    default:
+      console.log(69);
+      break;
+  }
+}
+*/
+//4
+/*
+function simpleSwitch() {
+  let id = 2;
+  switch (id) {
+    case 1:
+      console.log(1);
+      break;
+    case 2:
+      console.log(2);
+      break;
+    default:
+      console.log(69);
+      break;
+  }
+}
+*/
+//5
+/*
+function simpleSwitch() {
+  let id = 10;
+  switch (id) {
+    case 1:
+      console.log(1);
+      break;
+    case 2:
+      console.log(2);
+      break;
+    default:
+      console.log("Default");
+      break;
+  }
+}
+*/
+//6
+/*
+function simpleSwitch() {
+  let id = 22;
+  switch (id) {
+    case 1:
+      console.log(1);
+      break;
+    case 2:
+      console.log(2);
+      break;
+    default:
+      console.log(666);
+  }
+}
+*/
+//7
+/*
+function simpleSwitch() {
+  let id = 12;
+  switch (id) {
+    case 1:
+      console.log(1);
+      break;
+    case 2:
+      console.log(2);
+      break;
+    default:
+      console.log(69);
+  }
+}
+*/
+//8
+/*
+function simpleSwitch() {
+  let id = 20;
+  switch (id) {
+    case 1:
+      console.log(1);
+      break;
+    case 2:
+      console.log(2);
+      break;
+    default:
+      console.log("default");
+  }
+}
+*/
+//9
+/*
+function simpleSwitch() {
+  let id = 22;
+  switch (id) {
+    default:
+      console.log(69);
+      break;
+    case 1:
+      console.log(1);
+      break;
+    case 2:
+      console.log(2);
+      break;
+  }
+}
+*/
