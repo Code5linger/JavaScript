be560e5387269ff2fa53a1e72a7623d4afaba70c
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-09-07 04:28:52 +0600

    W

---
+++ [Pluralsight] JavaScript Core Language Path v2/02. Syntax and Operators 97'/javascript.js
@@ -0,0 +1,44 @@
+/*
+function simpleSwitch() {
+  let id = 3;
+
+  switch (id) {
+    case 1:
+      console.log(1);
+      break;
+    case 2:
+      console.log(2);
+      break;
+    case 3:
+      console.log(3);
+      break;
+
+    default:
+      console.log("Default");
+      break;
+  }
+}
+*/
+
+function simpleSwitch() {
+  let color = "Blue";
+
+  switch (color) {
+    case "Red":
+    case "Pink":
+      console.log("The color is red");
+    case "Blue":
+    case "Light Blue":
+    case "Dark Bue":
+      console.log("The color is blue");
+      break;
+    case "Gray":
+    case "Gray":
+      console.log("The color is Gray");
+      break;
+
+    default:
+      console.log("Unknown Color");
+      break;
+  }
+}
