969c47bbbd1333b372c49e516d0d11bc7654153b
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-09-20 22:57:41 +0600

    W

--- [Pluralsight] JavaScript Core Language Path v2/02. Syntax and Operators 97'/javascript.js
+++ [Pluralsight] JavaScript Core Language Path v2/02. Syntax and Operators 97'/javascript.js
@@ -59,3 +59,21 @@ function simpleSwitch() {
   }
 }
 */
+function simpleSwitch() {
+  let id = 10;
+
+  switch (id) {
+    default:
+      console.log("XP");
+      break;
+    case 1:
+      console.log("1");
+      break;
+    case 2:
+      console.log("2");
+      break;
+    case 3:
+      console.log("3");
+      break;
+  }
+}
