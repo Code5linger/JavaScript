cf83894f7fd4bb699c25f6a1493efe017a2ea256
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-10-31 16:26:02 +0600

    W

--- JavaScript Core Language Path by Pluralsight v3/01. Getting Started 231'/04 - Variables and Constants 25'🔷/script.js
+++ JavaScript Core Language Path by Pluralsight v3/01. Getting Started 231'/04 - Variables and Constants 25'🔷/script.js
@@ -1,2 +1,36 @@
 let test = 7;
 console.log(test);
+
+let string = "Test",
+  num = 17,
+  bool = true;
+console.log(string, num, bool);
+
+let _test = 69;
+console.log(_test);
+
+let $test = 420;
+console.log($test);
+
+let xi = 8;
+console.log(xi);
+
+let empty;
+console.log(empty);
+
+let _1st = 1;
+console.log(_1st);
+_1st = 2;
+console.log(_1st);
+
+let mt;
+console.log(mt + " 🤦‍♂️");
+mt = 1;
+console.log(mt + " 🤦‍♂️");
+mt = 69;
+console.log(mt + " 🤦‍♂️");
+
+const testA = 55;
+console.log(testA);
+testA = 69;
+console.log(testA);
