9d95ae3c0a24c89269abcb8c1682c89385804fbf
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-11-01 02:44:48 +0600

    W

---
+++ JavaScript Core Language Path by Pluralsight v3/01. Getting Started 231'/05 - Types and Operators 35' 🔷/script.js
@@ -0,0 +1,11 @@
+let num = 17.69;
+console.log(num);
+console.log(typeof num);
+
+let string = "Test";
+console.log(typeof string);
+
+num = num + 1;
+console.log(num);
+num = num - 2;
+console.log(num);
