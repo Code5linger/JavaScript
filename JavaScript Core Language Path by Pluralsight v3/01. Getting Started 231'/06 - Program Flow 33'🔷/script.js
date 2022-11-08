4ff98902accd395d3e1a1a6c0b4eb80f14ea88b6
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-11-08 16:44:28 +0600

    W

---
+++ JavaScript Core Language Path by Pluralsight v3/01. Getting Started 231'/06 - Program Flow 33'🔷/script.js
@@ -0,0 +1,26 @@
+if (5 >= 5) {
+  console.log("Yes");
+}
+
+let district = "Dhaka";
+let tax = 7;
+
+if (district === "xDhaka") {
+  tax = 0;
+}
+
+console.log(tax);
+
+let country = "BD",
+  love = 70;
+
+if (country == "BD") {
+  love = 100;
+}
+console.log(love);
+
+let value = 20;
+
+if (value > 15) {
+  console.log("Discounted");
+}
