9375960668ae2a6d71ba9e173943d2bc9dd319eb
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-11-11 17:05:29 +0600

    W

---
+++ JavaScript Core Language Path by Pluralsight v3/01. Getting Started 231'/06 - Program Flow 33'🔷/script1.js
@@ -0,0 +1,13 @@
+let numA = 14;
+console.log(numA);
+console.log(typeof numA);
+
+let strA = "Test";
+console.log(typeof strA);
+
+let numB = 19;
+numB += 1;
+console.log(numB);
+let numC = 70;
+numC -= 1;
+console.log(numC);
