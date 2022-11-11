81cf3dcfee460cd2813f776d3ae1291402c87792
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-11-11 17:33:33 +0600

    W

--- JavaScript Core Language Path by Pluralsight v3/01. Getting Started 231'/06 - Program Flow 33'🔷/script1.js
+++ JavaScript Core Language Path by Pluralsight v3/01. Getting Started 231'/06 - Program Flow 33'🔷/script1.js
@@ -11,3 +11,25 @@ console.log(numB);
 let numC = 70;
 numC -= 1;
 console.log(numC);
+let numD = 13;
+numD *= 6;
+console.log(numD);
+
+let numE = 9;
+console.log("++ " + ++numE);
+
+let numF = 9;
+console.log(numF++ + " ++");
+console.log(numF++);
+
+let numG = 10;
+console.log(numG-- + "--");
+console.log(numG-- + "--");
+console.log(numG);
+
+let numH = 10;
+console.log("-- " + --numH);
+console.log(numH);
+
+let numI = (3 + 2) * 2;
+console.log(numI); //Operator Precedence
