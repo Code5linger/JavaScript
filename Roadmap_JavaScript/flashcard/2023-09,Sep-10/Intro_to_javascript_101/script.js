10efc590b2c546643b2648a438513b79ae8e67a3
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2023-09-10 16:43:34 +0600

    Flashcard

---
+++ Roadmap_JavaScript/flashcard/2023-09,Sep-10/Intro_to_javascript_101/script.js
@@ -0,0 +1,59 @@
+// console.log("Hello from the other side");
+
+// alert("Hello World🌏");
+
+// let firstName = prompt("What is your name?");
+// alert("Hello there " + firstName + "👋");
+// Ite 1
+// let today = Date();
+// document.body.innerHTML = today;
+
+// let newParagraph = document.createElement("p");
+// let textForNewParagraph = document.createTextNode("Lorem Ipsum");
+
+// newParagraph.appendChild(textForNewParagraph);
+// document.body.appendChild(newParagraph);
+
+// Ite 2
+let today = Date();
+document.body.innerHTML = "<h1>Hi👋 Today is " + today + "</h1>";
+
+// Ite 3
+// document.body.innerHTML = "<h1>Hi! Today is " + Date() + "</h1>";
+
+// Ite 4
+// document.body.innerHTML = "<h1>Hi! Today is " + Date() + "</h1>";
+
+// Ite 5
+// document.body.innerHTML = "<h1>Hi! Today is " + Date() + "</h1>";
+
+// Ite 6
+// document.body.innerHTML = "<h4>Hi! Today is " + Date() + "</h4>";
+
+// Ite 7
+// document.body.innerHTML = "<h3>Hi! Today is " + Date() + "</h3>";
+
+// Ite 8
+// document.body.innerHTML = "<h4>Hi! Today is " + Date() + "</h4>";
+
+// Ite 9
+// document.body.innerHTML = "<h5>Hi! Today is " + Date() + "</h5>";
+
+// Ite 10
+// document.body.innerHTML = "<h2>Hi! Today is " + Date() + "</h2>";
+// document.body.style.background = "Crimson";
+// document.body.style.color = "White";
+// document.body.style.textAlign = "center";
+// document.body.style.padding = "10rem";
+// Ite 1
+// document.body.innerHTML = "<h2>Hi👋 Today is " + Date() + "</h2>";
+// document.body.style.background = "Black";
+// document.body.style.color = "White";
+// document.body.style.textAlign = "Center";
+// document.body.style.padding = "10rem";
+// Ite 2
+// document.body.innerHTML = "<h1>Hi! Today is " + Date() + "</h1>";
+// document.body.style.background = "Black";
+// document.body.style.color = "White";
+// document.body.style.textAlign = "Center";
+// document.body.style.padding = "10rem 0";
