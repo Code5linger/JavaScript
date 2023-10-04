1cc60ba7cb135eb344837e51759424f215b31754
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2023-10-04 16:26:09 +0600

    DOM

---
+++ Roadmap_JavaScript/0_DOM/script.js
@@ -0,0 +1,31 @@
+// console.log("Test");
+
+// document.title = "CopaSamsu🪓🪓";
+document.URL = "copasamsu.com";
+// console.log(document.URL);
+// console.clear();
+// let headerJS = document.getElementsByClassName(header);
+// console.log(header);
+
+const header = document.getElementsByTagName("h1");
+console.log(header[1]);
+
+const list = document.getElementsByClassName("item");
+console.log(list);
+
+console.log(header.length);
+console.log(list.length);
+
+const button = document.getElementById("submit");
+console.log(button);
+
+const headers = document.querySelectorAll("button");
+console.log(headers);
+const lists = document.querySelectorAll(".item");
+console.log(lists);
+const buttons = document.querySelectorAll("#submit");
+console.log(buttons);
+const test = document.querySelectorAll(".test");
+console.log(buttons);
+
+console.log(test.header);
