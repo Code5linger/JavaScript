44f80f561d887c101af29cfbc1d58a82d888d9aa
Code5linger <sakib.ahmed.1708@gmail.com>
2024-01-08 00:03:09 +0600

    !

---
+++ 🏭JavaScript Streamlined🪨/01/A/B1JavaScriptFundamentals/scrip.js
@@ -0,0 +1,14 @@
+let firstName = "David";
+let lastName = "Tucker";
+
+console.log(`Hello $(firstName) $(lastName)`);
+
+let int = 100;
+
+for (let i; i < 18; i++) {
+  console.log(int + int);
+}
+
+/*
+1️⃣⏺️1️⃣
+*/
