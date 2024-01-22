2cefced0d2092c6b46c605389ea93f3f581ef08e
Code5linger <sakib.ahmed.1708@gmail.com>
2024-01-22 23:37:22 +0600

    !

---
+++ 🪙Chapter 2 Variables, Data Types, Methods, and More/A/script.js
@@ -0,0 +1,133 @@
+// console.log(20, 'Code', true);
+
+// console.error('Error!');
+// console.warn('Warning!');
+
+// console.table({ name: 'code', age: 26 });
+
+// console.groupCollapsed();
+// console.log(20, 'Code', true);
+// console.warn('Warning!');
+// console.groupEnd();
+// console.error('Error!');
+
+// const style = 'padding : 10px; background: white; color: black';
+
+// console.log('%cHello World', style);
+// console.log('%cHello World', style);
+// console.log('%cHello World', style);
+
+// let firstName = 'Code';
+// let lastName = 'Slinger';
+
+// console.log(firstName, lastName, age);
+
+// let age = 30;
+// console.log(age);
+
+// age = 31;
+
+// console.log(age);
+
+// score = 1;
+
+// console.log(score);
+
+// if (true) {
+//   score = score + 1;
+// }
+
+// console.log(score);
+
+// const arr = [1, 2, 3, 4];
+
+// arr.push(5);
+
+// console.log(arr);
+
+// const person = {
+//   name: 'Code',
+// };
+
+// person.name = 'Slinger';
+
+// console.log(person.name);
+
+// const x = 100;
+// x = 150;
+
+// console.log(x);
+
+// const arr = [1, 2, 3, 4];
+
+// console.log(arr);
+
+// arr.push(5);
+
+// console.log(arr);
+
+// const person = {
+//   name: 'Elrick',
+//   age: 30,
+//   email: 'elrick@mail.com',
+// };
+
+// console.table(person);
+
+// person.name = 'Ashford';
+// person.email = 'ashford@mail.com';
+// person.age = 26;
+
+// console.log(person);
+// console.table(person);
+
+// // let a, b, c;
+
+// const a = 16,
+//   b = 'code',
+//   c = true;
+
+// Strings
+
+const firstName = 'Code';
+
+// Number
+
+const age = 29;
+const temp = 98.9;
+
+// Boolean
+
+const hasGF = false;
+
+// NUll
+
+const aptNum = null;
+
+// Undefined
+
+const score = undefined;
+
+// Symbol
+
+const id = Symbol('id');
+
+// BigInt
+
+const n = 90000000000000n;
+
+// Ref Type
+
+const numbers = [1, 2, 3, 4, 5];
+
+const person = {
+  name: 'Code',
+};
+
+function sayHello() {
+  console.log('Hello');
+}
+
+const output = sayHello;
+
+console.log(output, typeof output);
