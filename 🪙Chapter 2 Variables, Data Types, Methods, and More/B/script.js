e6680042998cd90d1ee151a04bf6a4bf6665f5cb
Code5linger <sakib.ahmed.1708@gmail.com>
2024-01-24 00:27:08 +0600

    !

---
+++ 🪙Chapter 2 Variables, Data Types, Methods, and More/B/script.js
@@ -0,0 +1,38 @@
+// String
+const firstName = 'Elrick ';
+
+// Number
+const age = 30;
+
+// Boolean
+const bool = true;
+
+// Null
+const hasGF = null;
+
+// Undefined
+const score = undefined;
+
+// Symbol
+const id = Symbol('id');
+
+// BigInt
+const num = 9000000000000000000000n;
+
+// Reference Types
+// Array
+const Numbers = [1, 2, 3, 4];
+
+//Object Leteral
+const person = {
+  name: 'Erlick',
+};
+
+// Function
+function sayHello() {
+  console.log('Hello');
+}
+
+const output = sayHello;
+
+console.log(output, typeof output);
