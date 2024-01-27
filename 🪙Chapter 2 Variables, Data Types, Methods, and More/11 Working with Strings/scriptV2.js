4f943692c97b239e528a52e636f9105223b44c94
Code5linger <sakib.ahmed.1708@gmail.com>
2024-01-27 23:55:34 +0600

    !

---
+++ 🪙Chapter 2 Variables, Data Types, Methods, and More/11 Working with Strings/scriptV2.js
@@ -0,0 +1,13 @@
+let x;
+
+// const name = 'Elrick';
+// const age = 29;
+
+// x = `Hello, my name is ${name} I am ${age}`;
+// x = typeof s;
+
+// x = s.toUpperCase();
+
+const s = 'Hello World';
+
+console.log(s.valueOf());
