4a34e6f835317bbd5169c86c8218acb3e434634c
Code5linger <sakib.ahmed.1708@gmail.com>
2024-01-28 07:16:52 +0600

    13 Working with Numbers

---
+++ 🪙Chapter 2 Variables, Data Types, Methods, and More/13 Working with Numbers/script.js
@@ -0,0 +1,27 @@
+let x = Math.floor(Math.random() * 100 + 1);
+let y = Math.floor(Math.random() * 50 + 1);
+
+// Get the sum
+const sum = x + y;
+const sumOutput = `${x} + ${y} = ${sum}`;
+console.log(sumOutput);
+
+// Get the difference
+const diff = x - y;
+const differenceOutput = `${x} - ${y} = ${diff}`;
+console.log(differenceOutput);
+
+// Get the product
+const prod = x * y;
+const productOutput = `${x} * ${y} = ${prod}`;
+console.log(productOutput);
+
+//  Get the quotient
+const quot = x / y;
+const quotientOutput = `${x} / ${y} = ${quot}`;
+console.log(quotientOutput);
+
+// Get the reminder
+const rm = x % y;
+const rmOutput = `${x} % ${y} = ${rm}`;
+console.log(rmOutput);
