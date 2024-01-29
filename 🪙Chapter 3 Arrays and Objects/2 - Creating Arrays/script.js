3c7c3df9d1638757a2812f3f20d0bc1b7436ab35
Code5linger <sakib.ahmed.1708@gmail.com>
2024-01-29 08:30:03 +0600

    !

---
+++ 🪙Chapter 3 Arrays and Objects/2 - Creating Arrays/script.js
@@ -0,0 +1,17 @@
+// Array Literal
+
+// console.log(numbers);
+
+// Array Constructor
+const fruits = new Array('apple', 'grape', 'orange', 69, 39, 69, 420);
+
+const numbers = [12, 45, 33, 29];
+
+x = `My age is ${numbers[3]}`;
+
+const mixed = [69, 'CopaSamsu', true, null, undefined];
+
+mixed[mixed.length - 1] = 'Test';
+
+console.log(mixed);
+// (5) [69, 'CopaSamsu', true, null, 'Test']
---
+++ 🪙Chapter 3 Arrays and Objects/3 - Basic Array Methods/script.js
@@ -0,0 +1,4 @@
+const arr = [12, 56, 89, 11, 69];
+
+console.log(arr.splice(0, 2));
+console.log(arr);
