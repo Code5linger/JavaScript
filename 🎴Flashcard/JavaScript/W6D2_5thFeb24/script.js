5f66a1189e008d03c855b87ea4b48c42ba819cac
Code5linger <sakib.ahmed.1708@gmail.com>
2024-02-05 15:16:57 +0600

    !

---
+++ 🎴Flashcard/JavaScript/W6D2_5thFeb24/script.js
@@ -0,0 +1,97 @@
+// 2
+
+// let hourlyWage = 7.25;
+
+// let numOfHourWorked = 40;
+
+// console.log(hourlyWage * numOfHourWorked);
+
+// 2.4
+// let fName = 'Elrick';
+// console.log(fName);
+
+// 2.34
+// console.log('String', null, undefined, 69, true, 2.66);
+
+// 2.20
+let para = 'This is a paragraph!';
+
+console.log('<p>' + para + '</p>');
+
+// 2.3
+// let variable = 'Test';
+
+// console.log(variable);
+
+// 2.39
+const style = 'padding: 10px; background: white, color: black';
+
+console.log('%cHello World', style);
+
+// 2.11
+let swarma = 150;
+
+let totalTax = swarma * 0.085;
+
+console.log(swarma + totalTax);
+
+// 2.23
+
+// 2.32
+// console.log(Date());
+
+// 2.7
+// Name can't start with a number
+// 1st word
+
+// 2.50
+// const array = [1, 2, 3, 4, 5];
+
+// console.log(array);
+
+// 2.51
+// const person = {
+//   name: 'Elrick',
+//   age: 30,
+//   email: 'elrick@gmail.com',
+// };
+
+// person.name = 'Ashford';
+// person.age = 69;
+// person.email = 'ashford@gmail.com';
+
+// console.log(person.name, person.age, person.email);
+
+// 2.49
+score = 1;
+
+console.log(score);
+
+if (true) {
+  score = score + 1;
+}
+
+console.log(score);
+
+// 2.27
+
+// 2.5
+// let text = 'Elrick';
+// console.log('Hello ' + text + '👋');
+
+// 2.36
+console.warn('Brooo...');
+
+// console.alert('Dude...');
+
+// 2.19
+let longInt = Math.PI;
+console.log(longInt);
+console.log(Math.round(longInt));
+console.log(Math.ceil(longInt));
+
+// 2.13
+let test;
+console.log(test);
+
+let variable = 69;
--- 🪙Chapter 3 Arrays and Objects/6 - Object Spread Operator and Methods/script.js
+++ 🪙Chapter 3 Arrays and Objects/6 - Object Spread Operator and Methods/script.js
@@ -1,29 +1,57 @@
-const todo = new Object();
+// const todo = new Object();
 
-todo.id = 69;
-todo.test = true;
-todo.user = 'Cod35linger';
+// todo.id = 69;
+// todo.test = true;
+// todo.user = 'Cod35linger';
 
-const person = {
-  address: {
-    coords: {
-      lat: 42.46464654,
-      lng: -71.3546847,
-    },
-  },
-};
+// const person = {
+//   address: {
+//     coords: {
+//       lat: 42.46464654,
+//       lng: -71.3546847,
+//     },
+//   },
+// };
 
-const obj1 = { a: 1, b: 2, x: 6 };
-const obj2 = { c: 3, d: 4, y: 9 };
+// const obj1 = { a: 1, b: 2, x: 6 };
+// const obj2 = { c: 3, d: 4, y: 9 };
 
-const obj4 = Object.assign({}, obj1, obj2);
+// const obj4 = Object.assign({}, obj1, obj2);
 
-console.log(Object.keys(todo));
+// console.log(Object.keys(todo));
 
-console.log(Object.keys(todo).length);
+// console.log(Object.keys(todo).length);
 
-console.log(Object.values(todo));
+// console.log(Object.values(todo));
 
-console.log(Object.entries(todo));
+// console.log(Object.entries(todo));
 
-console.log(todo.hasOwnProperty('name'));
+// console.log(todo.hasOwnProperty('name'));
+
+// const todo = {};
+
+// todo.id = 69;
+// todo.test = true;
+// todo.naame = 'CodeSlinger';
+
+// console.log(todo, typeof todo);
+
+// const obj1 = { a: 1, b: 2, x: 6 };
+// const obj2 = { c: 3, d: 4, y: 9 };
+
+// const obj3 = { ...obj1, ...obj2 };
+
+// const obj4 = Object.assign({}, obj1, obj2);
+// console.log(obj3, typeof obj3);
+// console.log(obj4, typeof obj4);
+
+const todos = [
+  { id: 1, name: '1' },
+  { id: 2, name: '2' },
+  { id: 3, name: '3' },
+];
+
+console.log(todos);
+
+let x = todos[0];
+console.log(x);
