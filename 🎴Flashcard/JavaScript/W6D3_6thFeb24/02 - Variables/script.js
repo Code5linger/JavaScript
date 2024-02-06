c1e3dc175627e09125dadd212379f5486c53f010
Code5linger <sakib.ahmed.1708@gmail.com>
2024-02-06 14:51:28 +0600

    !

---
+++ 🎴Flashcard/JavaScript/W6D3_6thFeb24/02 - Variables/script.js
@@ -0,0 +1,44 @@
+// 7
+// Only lerrers, numbers, underscores & doller signes
+// Can't start with a number
+// no space between variable name
+
+// 2.17
+const hourlyWages = 7.69;
+const numOfHours = 40;
+
+const totalWages = hourlyWages * numOfHours;
+
+console.log('Total Wages $' + totalWages);
+
+// 2.19
+const num = 3.14164646431616861;
+
+// 2.20
+
+// 2.23
+// Name starts with a lowercase letter
+// Use Camel Case
+
+// 2.27
+let name = 'Elrick';
+// let name = 'ashford';
+
+// 2.36
+console.warn('Warning');
+
+// 2.39
+const style = 'padding: 1rem; color: white; border: 2px solid white ';
+
+console.log('%cHello World', style);
+
+// 49
+score = 69;
+
+console.log(score);
+
+if ((score = true)) {
+  score = score + 1;
+}
+
+console.log(score);
