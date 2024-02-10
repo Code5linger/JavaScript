985cfd8e5e3eab2030319b54f20d4f3747666ac2
Code5linger <sakib.ahmed.1708@gmail.com>
2024-02-10 06:47:19 +0600

    !

--- 🎴Flashcard/JavaScript/W6D6_9thFeb24/JS-03-06-Object-Spread-Operator-and-Methods/script_2.js
+++ 🎴Flashcard/JavaScript/W6D6_9thFeb24/JS-03-06-Object-Spread-Operator-and-Methods/script_2.js
@@ -1,1 +1,27 @@
-console.log('Test');
+const todo = Object();
+
+todo.name = 'Code';
+todo.id = 69;
+
+console.log(todo, typeof todo);
+
+// 02
+const objA = { a: 1, b: 2 };
+const objB = { c: 3, d: 4 };
+
+const objX = { ...objA, ...objB };
+
+console.log(objX);
+
+const objY = Object.assign({}, objA, objB);
+
+console.log(objY);
+
+// 04
+console.log(Object.keys(objX));
+console.log(Object.values(objY));
+console.log(Object.entries(objX));
+
+console.log(Object.entries(objX).length);
+
+console.log(objX.hasOwnProperty('a'));
---
+++ 🎴Flashcard/JavaScript/W6D6_9thFeb24/JS-03-08-Destructuring-and-Naming/script.js
@@ -0,0 +1,12 @@
+const firstName = 'Code';
+const lastname = 'Ashford';
+var age = 69;
+const id = 17;
+
+const person = {
+  firstName: firstName,
+  lastname: lastname,
+  age,
+};
+
+console.log(person.age);
