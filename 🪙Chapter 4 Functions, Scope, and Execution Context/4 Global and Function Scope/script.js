414b5059d83978f32a7b2022763c5f77c5f24ddb
Code5linger <sakib.ahmed.1708@gmail.com>
2024-02-12 21:02:18 +0600

    5

--- 🪙Chapter 4 Functions, Scope, and Execution Context/4 Global and Function Scope/script.js
+++ 🪙Chapter 4 Functions, Scope, and Execution Context/4 Global and Function Scope/script.js
@@ -2,25 +2,50 @@
 // window.alert('Hello');
 // console.log(window.innerWidth, window.innerHeight);
 
-let x = 100;
+// let x = 100;
 
-console.log(x, ' in Global');
+// console.log(x, ' in Global');
+
+// function run() {
+//   let x = 69;
+//   console.log(window.innerHeight);
+//   console.log(x, ' in Function');
+// }
+
+// console.log(x, ' in Global❗');
+
+// run();
+// console.log(x, ' in Global');
+
+// if (true) {
+//   let y = 420;
+//   console.log(x, ' in block');
+//   console.log(y);
+// }
+
+// console.log(y);
+
+let x = 100;
+console.log(x + ' in Global');
+// 100 in Global;
 
 function run() {
-  let x = 69;
-  console.log(window.innerHeight);
-  console.log(x, ' in Function');
+  console.log(x + ' in Function');
 }
-
-console.log(x, ' in Global❗');
 
 run();
-console.log(x, ' in Global');
+// 100 in Function
 
 if (true) {
+  console.log(x + ' in Conditional Block');
+}
+// 100 in Conditional Block
+
+function add() {
+  let x = 1;
   let y = 420;
-  console.log(x, ' in block');
-  console.log(y);
+  console.log(x + y + ' in Function');
 }
 
-console.log(y);
+add();
+// 421 in Function;
