cefe2dedaeaed7f76b56fc264e6b595b60574acc
Code5linger <sakib.ahmed.1708@gmail.com>
2024-02-14 22:07:04 +0600

    !

---
+++ 🪙Chapter 4 Functions, Scope, and Execution Context/11 - Execution Context/script.js
@@ -0,0 +1,28 @@
+// const x = 100;
+// const y = 50;
+
+// function getSome(n1, n2) {
+//   const sum = n1 + n2;
+//   return sum;
+// }
+
+// const sum1 = getSome(x, y);
+// const sum2 = getSome(10, 5);
+
+// console.log(sum1, sum2);
+
+function first() {
+  console.log('1st');
+}
+
+function second() {
+  console.log('2nd');
+}
+
+function third() {
+  console.log('3rd');
+}
+
+first();
+second();
+third();
---
+++ 🪙Chapter 5 Logic and Control Flow/01-if-statement/script.js
@@ -0,0 +1,16 @@
+if (true) {
+  console.log('This is true');
+}
+
+const x = 10;
+const y = 5;
+
+if (x > y) {
+  console.log(`${x} is greater than ${y}`);
+}
+if (x === y) {
+  console.log(`${x} is equal than ${y}`);
+}
+if (x < y) {
+  console.log(`${x} is smaller than ${y}`);
+}
