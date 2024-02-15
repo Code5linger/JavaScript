6bfca1fe0a45b333629d53def3c9309fc42dd45c
Code5linger <sakib.ahmed.1708@gmail.com>
2024-02-15 23:34:24 +0600

    !

---
+++ 🪙Chapter 5 Logic and Control Flow/07 - Logical Operators/script.js
@@ -0,0 +1,34 @@
+// console.log(10 < 20 && 30 > 15 && 40 > 30);
+
+// console.log(10 > 20 || 30 < 15);
+
+// && - Will return first falsy value or the lst value
+// let a;
+// a = 10 && false && '' && 0 && 30;
+
+// console.log(a);
+// false
+
+// const posts = ['Post 1', 'Post 2'];
+
+// const posts = [];
+// posts.length > 0 && console.log(posts[0]);
+
+// undefined
+
+// || - Will return the first truthy value or the last value
+
+// let b;
+// b = 0 || '' || null || undefined;
+// console.log(b);
+// null
+
+// ?? Returns the right side operand when the left is null or undefined
+
+let c;
+
+c = '' ?? 60;
+
+console.log(c);
+//
+// '' is not null or undefined
