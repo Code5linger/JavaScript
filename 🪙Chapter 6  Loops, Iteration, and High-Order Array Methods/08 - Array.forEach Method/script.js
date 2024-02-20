fc35ee0850a1706ca4931e8fd37c57b140c95df9
Code5linger <sakib.ahmed.1708@gmail.com>
2024-02-20 17:04:04 +0600

    08 - Array forEach Method

--- 🪙Chapter 6  Loops, Iteration, and High-Order Array Methods/08 - Array.forEach Method/script.js
+++ 🪙Chapter 6  Loops, Iteration, and High-Order Array Methods/08 - Array.forEach Method/script.js
@@ -1,5 +1,37 @@
-const array = ['a', 'b', 'c'];
+// const array = ['a', 'b', 'c'];
 
-array.forEach((element) => console.log(element));
+// array.forEach((element) => console.log(element));
 
-// console.log(array.__proto__);
+// // console.log(array.__proto__);
+
+const alphabets = ['a', 'b', 'c', 'd', 'e'];
+
+// console.log(alphabets.__proto__);
+
+// alphabets.forEach(function (word) {
+//   console.log(word);
+// });
+
+// alphabets.forEach((word, index, arr) => {
+//   console.log(`${index} - ${word}`, arr);
+// });
+
+// 0 - a (5) ['a', 'b', 'c', 'd', 'e']
+
+// function logAlphabets(word) {
+//   console.log(word);
+// }
+
+// logAlphabets(alphabets);
+
+// alphabets.forEach(logAlphabets);
+
+const socialObj = [
+  { name: 'A', url: '#A' },
+  { name: 'B', url: '#B' },
+  { name: 'C', url: '#C' },
+  { name: 'D', url: '#D' },
+  { name: 'E', url: '#E' },
+];
+
+socialObj.forEach((item) => console.log(item.url));
