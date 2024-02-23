e153a47b43ae1d8806a9017af8a287808dd44a89
Code5linger <sakib.ahmed.1708@gmail.com>
2024-02-23 22:21:25 +0600

    10 - Refactor to Multiple Functions

---
+++ 🪙Chapter 7  DOM Manipulation/10 - Refactor to Multiple Functions/shopping-list/script.js
@@ -0,0 +1,45 @@
+// Quick
+// function createListItem(item) {
+//   const li = document.createElement('li');
+
+//   li.innerHTML = `${item}
+//           <button class="remove-item btn-link text-red">
+//             <i class="fa-solid fa-xmark"></i>
+//           </button>`;
+
+//   document.querySelector('.items').appendChild(li);
+// }
+
+// Better
+function createNewItem(item) {
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(item));
+
+  const button = createButton('remove-item btn-link text-red');
+
+  li.appendChild(button);
+
+  console.log(li.innerHTML);
+
+  document.querySelector('.items').appendChild(li);
+}
+
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+
+  const icon = createIcon('fa-solid fa-xmark');
+
+  button.appendChild(icon);
+
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
+
+// createListItem('Eggs');
+createNewItem('Biriani🍚');
