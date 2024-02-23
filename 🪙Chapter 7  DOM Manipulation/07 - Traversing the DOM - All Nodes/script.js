56aa315995f6f69300bd8cb68e92c0aa0307ed77
Code5linger <sakib.ahmed.1708@gmail.com>
2024-02-23 19:47:43 +0600

    07 - Traversing the DOM - All Nodes

---
+++ 🪙Chapter 7  DOM Manipulation/07 - Traversing the DOM - All Nodes/script.js
@@ -0,0 +1,30 @@
+const parent = document.querySelector('.parent');
+
+// console.log(parent.childNodes);
+
+// console.log(parent.childNodes[1].textContent);
+
+// console.log(parent.childNodes[1].nodeName);
+
+// console.log(parent.childNodes[1].innerHTML);
+
+// console.log(parent.childNodes[1].outerHTML);
+
+// parent.childNodes[3].innerHTML = '🐔';
+
+// parent.childNodes[5].style.color = 'crimson';
+
+// console.log((parent.firstChild.textContent = '1️⃣'));
+
+// console.log((parent.lastChild.textContent = '2️⃣'));
+
+// Parent Node
+
+// const child = document.querySelector('.child');
+
+// child.parentNode.style.backgroundColor = 'gray';
+// child.parentNode.style.padding = '1rem';.
+
+const secontItem = document.querySelector('.child:nth-child(2)');
+
+console.log(secontItem.previousSibling);
