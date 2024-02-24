2dfa6ea7f0b7855108a2e483bdd8919e8811507e
Code5linger <sakib.ahmed.1708@gmail.com>
2024-02-25 00:35:50 +0600

    !

--- 🪙Chapter 7  DOM Manipulation/12 - Custom insertAfter() Challenge/shopping-list/script.js
+++ 🪙Chapter 7  DOM Manipulation/12 - Custom insertAfter() Challenge/shopping-list/script.js
@@ -7,25 +7,36 @@
 
 // console.log(firstItem);
 
-function insertAfter(element, text) {
-  const existingEl = document.querySelector(element);
+// function insertAfter(element, text) {
+//   const existingEl = document.querySelector(element);
 
-  const newEl = document.createElement('li');
-  newEl.innerHTML = `${text}
-  <button class="remove-item btn-link text-red">
-    <i class="fa-solid fa-xmark"></i>
-  </button>`;
+//   const newEl = document.createElement('li');
+//   newEl.innerHTML = `${text}
+//   <button class="remove-item btn-link text-red">
+//     <i class="fa-solid fa-xmark"></i>
+//   </button>`;
 
-  console.log(newEl);
-  newEl.insertAdjacentElement('afterend', existingEl);
+//   console.log(newEl);
+//   newEl.insertAdjacentElement('afterend', existingEl);
 
-  document.querySelector(element).appendChild(newEl);
-}
+//   document.querySelector(element).appendChild(newEl);
+// }
 
-insertAfter('ul', '🅰️');
-insertAfter('body', '🅰️');
-insertAfter('ul', '🅰️');
-insertAfter('ul', '🅰️');
-insertAfter('ul', '🅰️');
+// insertAfter('ul', '🅰️');
+// insertAfter('body', '🅰️');
+// insertAfter('ul', '🅰️');
+// insertAfter('ul', '🅰️');
+// insertAfter('ul', '🅰️');
 // insertAfter('li', '🅱️');
 // insertAfter('.filter', '©️');
+
+function insertAfter(newEl, existingEl) {
+  existingEl.parentElement.insertBefore(newEl, existingEl.nextSibling);
+}
+
+const li = document.createElement('li');
+li.textContent = 'Insert Me After!';
+
+const firstItem = document.querySelector('li:first-child');
+
+insertAfter(li, firstItem);
