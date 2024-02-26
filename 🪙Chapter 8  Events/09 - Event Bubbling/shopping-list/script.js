37734c8a03ba3f265d59a9f16d71fc5d1549642e
Code5linger <sakib.ahmed.1708@gmail.com>
2024-02-27 00:15:17 +0600

    !

---
+++ 🪙Chapter 8  Events/09 - Event Bubbling/shopping-list/script.js
@@ -0,0 +1,37 @@
+// const button = document.querySelector('form button');
+// const div = document.querySelector('form div:nth-child(2)');
+// const form = document.querySelector('form');
+
+// button.addEventListener('click', (e) => {
+//   alert('Button was clicked');
+//   e.stopPropagation();
+// });
+
+// div.addEventListener('click', () => {
+//   alert('Div was clicked');
+// });
+
+// form.addEventListener('click', () => {
+//   alert('Form was clicked');
+// });
+
+// document.body.addEventListener('click', () => {
+//   alert('Body was clicked');
+// });
+
+const button = document.querySelector('form button');
+const div = document.querySelector('form div:nth-child(2)');
+const form = document.querySelector('form');
+
+button.addEventListener('click', (event) => {
+  alert('🔽');
+  event.stopPropagation();
+});
+
+div.addEventListener('click', () => {
+  alert('🔻');
+});
+
+form.addEventListener('click', () => {
+  alert('⏬');
+});
