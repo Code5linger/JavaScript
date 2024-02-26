ce1e466e25bc159493526bc81bcfda0a052873b2
Code5linger <sakib.ahmed.1708@gmail.com>
2024-02-26 18:15:07 +0600

    04 - The Event Object

--- 🪙Chapter 8  Events/04 - The Event Object/shopping-list/script.js
+++ 🪙Chapter 8  Events/04 - The Event Object/shopping-list/script.js
@@ -1,9 +1,38 @@
 const logo = document.querySelector('img');
 
-function onClick(e) {
-  console.log(e);
+function onClick(event) {
+  // console.log(event.target);
+  // console.log(event.currentTarget);
+  // event.target.style.backgroundColor = 'black';
+  // console.log(event.type);
+  // console.log(event.timeStamp);
+  // console.log(event.clientX);
+  // console.log(event.clientY);
+  // console.log(event.offsetX);
+  // console.log(event.offsetY);
+  // console.log(event.pageX);
+  // console.log(event.pageY);
+  // console.log(event.screenX);
+  // console.log(event.screenY);
 }
 
-logo.addEventListener('click', function (e) {
-  console.log(e);
-});
+function onDrag(event) {
+  document.querySelector(
+    'h1'
+  ).textContent = `X ${event.clientX} Y ${event.clientY}`;
+}
+
+logo.addEventListener('click', onClick);
+logo.addEventListener('drag', onDrag);
+
+// document.body.addEventListener('click', function (event) {
+//   console.log(event.target);
+//   console.log(event.currentTarget);
+// });
+
+// function linkCloser(event) {
+//   event.preventDefault();
+//   console.log('Link was clicked🖱️');
+// }
+
+// document.querySelector('a').addEventListener('click', linkCloser);
