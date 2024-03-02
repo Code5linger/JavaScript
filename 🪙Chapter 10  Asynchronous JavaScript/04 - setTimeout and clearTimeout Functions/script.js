af00c70366e8215580028b6b8f8ad05d2336fd6c
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-02 18:59:59 +0600

    !

--- 🪙Chapter 10  Asynchronous JavaScript/04 - setTimeout and clearTimeout Functions/script.js
+++ 🪙Chapter 10  Asynchronous JavaScript/04 - setTimeout and clearTimeout Functions/script.js
@@ -2,13 +2,29 @@
 
 // console.log('🎴');
 
+// function changeText() {
+//   document.querySelector('h1').textContent = '💥';
+// }
+
+// const timerId = setTimeout(changeText, 3000);
+
+// document.querySelector('#cancel').addEventListener('click', () => {
+//   clearTimeout(timerId);
+//   console.log('⏱️');
+// });
+
+// function changeText() {
+//   document.querySelector('h1').textContent = '🌀';
+// }
+
+// const timerId = setTimeout(changeText, 3000);
+
+// document.querySelector('#cancel').addEventListener('click', () => {
+//   clearTimeout(timerId), console.log('⏱️🔨');
+// });
+
+setTimeout(changeText, 2000);
+
 function changeText() {
-  document.querySelector('h1').textContent = '💥';
+  document.querySelector('h1').textContent = '🎴';
 }
-
-const timerId = setTimeout(changeText, 3000);
-
-document.querySelector('#cancel').addEventListener('click', () => {
-  clearTimeout(timerId);
-  console.log('⏱️');
-});
