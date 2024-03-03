2c4bc60edc64e65dcbe97bb70cf5d5a3c418acc0
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-04 03:36:41 +0600

    📆04-03-2024

---
+++ 🪙Chapter 3 Arrays and Objects/3 - Basic Array Methods/B/script.js
@@ -0,0 +1,33 @@
+const array = [12, 56, 89, 11, 69];
+
+array.push('🅰️'); //01
+// console.log(array);
+array.pop(); //02
+// console.log(array);
+array.unshift('💤'); //03
+// console.log(array);
+array.shift(); //04
+// console.log(array);
+// array.reverse(); //05
+// console.log(array);
+
+// console.log(array.includes(56)); //06
+// console.log(array.includes(69)); //07   ❓
+
+// console.log(array.slice(0, 3)); //08    //09
+// console.log(array);
+
+// console.log(array.splice(4)); //10
+// console.log(array);
+
+// console.log(array);
+// console.log(array.splice(0, 3)); //11
+// console.log(array);
+
+// console.log(array);
+
+// console.log(array.splice(1, 4).reverse().toString().charAt(1));
+
+// console.log(array);
+
+console.log(array.indexOf(69));
