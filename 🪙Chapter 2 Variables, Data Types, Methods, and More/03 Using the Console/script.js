2de1f4eae6a11fc4f591422bfb6d572b2b3762dc
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-05 05:34:30 +0600

    !

---
+++ 🪙Chapter 2 Variables, Data Types, Methods, and More/03 Using the Console/script.js
@@ -0,0 +1,13 @@
+// console.log('String', 69, true, null, undefined);
+
+console.error('💥');
+
+console.warn('⚠️');
+
+console.table({ firstName: 'Elick', lastName: 'Ashford', age: 29 });
+console.groupCollapsed('🪬');
+console.groupEnd();
+
+const style = 'padding: 10px; border: 2px solid crimson; color: crimson';
+
+console.log('%cHello World', style);
