f6f9f225b4cdf82a4c23e3e86a06e8cb89220521
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-05 05:55:47 +0600

    !

--- 🪙Chapter 2 Variables, Data Types, Methods, and More/03 Using the Console/script.js
+++ 🪙Chapter 2 Variables, Data Types, Methods, and More/03 Using the Console/script.js
@@ -1,13 +1,38 @@
 // console.log('String', 69, true, null, undefined);
 
-console.error('💥');
+// console.error('💥');
 
+// console.warn('⚠️');
+
+// console.table({ firstName: 'Elick', lastName: 'Ashford', age: 29 });
+// console.groupCollapsed('🪬');
+// console.groupEnd();
+
+// const style = 'padding: 10px; border: 2px solid crimson; color: crimson';
+
+// console.log('%cHello World', style);
+
+console.groupCollapsed('🪬');
+
+console.log(69, typeof 69);
+console.log('string', typeof 'string');
+console.log('string', 69, true, undefined, null);
+
+let x = 420;
+console.log(x);
+
+console.error('💥');
 console.warn('⚠️');
 
-console.table({ firstName: 'Elick', lastName: 'Ashford', age: 29 });
-console.groupCollapsed('🪬');
+console.table({
+  firstName: 'Elrick',
+  lstName: 'Asford',
+  age: 69,
+  firstName: 'ElrickAA',
+  age: 420,
+});
+
 console.groupEnd();
 
-const style = 'padding: 10px; border: 2px solid crimson; color: crimson';
-
+let style = 'padding: 1rem; border: 2px solid crimson; border-radius: .5rem';
 console.log('%cHello World', style);
