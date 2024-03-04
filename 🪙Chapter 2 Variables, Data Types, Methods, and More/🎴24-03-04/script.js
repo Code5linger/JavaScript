9ba2d363794a248d6585448e433739e6abbc57ce
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-05 00:36:58 +0600

    🎴

---
+++ 🪙Chapter 2 Variables, Data Types, Methods, and More/🎴24-03-04/script.js
@@ -0,0 +1,95 @@
+// let 69Copa = 69
+
+let firstName = 'Elrick';
+let lastName = 'Ashford';
+
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+console.log(Date());
+console.log(new Date());
+
+console.log('string', 69, true, undefined, null);
+console.warn('💥');
+
+// ❓
+// const style = 'padding: 10px; background: white; color: black';
+// console.log('%cHello World', style);
+
+const style =
+  'padding: 1rem; border: 3px solid crimson; border-radius: .5rem; color: crimson';
+
+console.log('%cHello World', style);
+
+let swarmaPrice = 150;
+let vat = 8.5;
+
+let totalSwarmaPrice = swarmaPrice + (swarmaPrice / 100) * vat;
+console.log(totalSwarmaPrice);
+
+// let swarmaPrice = 150;
+// let vatAmmount = 0.085;
+// let totalTax = swarmaPrice * vatAmmount;
+
+// console.log(totalTax + swarmaPrice);
+
+// let empty;
+
+// console.log(empty);
+//
+let hourlyWages = 39.52914651651;
+let hourWorked = 8;
+let totalWages = hourWorked * hourlyWages;
+
+console.log(totalWages + '৳');
+console.log(totalWages.toFixed(2));
+
+let variable = 69;
+console.log(variable);
+variable = 420;
+console.log(variable);
+
+score = 1;
+console.log(score);
+
+if (true) {
+  score = score + 1;
+}
+
+console.log(score);
+
+const arr = [1, 2, 3, 4];
+console.log(arr);
+
+arr.push(5);
+console.log(arr);
+
+const person = {
+  firstName: 'Sakib',
+  lastName: 'Ahmed',
+  age: 29,
+};
+console.groupCollapsed('🪬');
+
+console.log(person);
+person.firstName = 'Elrick';
+person.lastName = 'Ashford';
+person.age = 69;
+
+console.log(person);
+
+console.log('Hello World!');
+
+let text = 'Hello World';
+
+console.log(text);
+
+let name = 'Sakib';
+console.log('Hello ' + name + '👋');
+
+// alert('💥');
+
+console.error('🌋');
+
+// console.groupEnd();
