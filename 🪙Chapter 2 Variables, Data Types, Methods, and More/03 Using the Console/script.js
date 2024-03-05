9cda7cee24ef2a9c378aeb97fcde1194948bd778
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-06 00:46:12 +0600

    🎴

--- 🪙Chapter 2 Variables, Data Types, Methods, and More/03 Using the Console/script.js
+++ 🪙Chapter 2 Variables, Data Types, Methods, and More/03 Using the Console/script.js
@@ -12,27 +12,35 @@
 
 // console.log('%cHello World', style);
 
-console.groupCollapsed('🪬');
+// console.groupCollapsed('🪬');
 
-console.log(69, typeof 69);
-console.log('string', typeof 'string');
-console.log('string', 69, true, undefined, null);
+// console.log(69, typeof 69);
+// console.log('string', typeof 'string');
+// console.log('string', 69, true, undefined, null);
 
-let x = 420;
-console.log(x);
+// let x = 420;
+// console.log(x);
 
-console.error('💥');
-console.warn('⚠️');
+// console.error('💥');
+// console.warn('⚠️');
 
-console.table({
-  firstName: 'Elrick',
-  lstName: 'Asford',
-  age: 69,
-  firstName: 'ElrickAA',
-  age: 420,
-});
+// console.table({
+//   firstName: 'Elrick',
+//   lstName: 'Asford',
+//   age: 69,
+//   firstName: 'ElrickAA',
+//   age: 420,
+// });
 
-console.groupEnd();
+// console.groupEnd();
 
-let style = 'padding: 1rem; border: 2px solid crimson; border-radius: .5rem';
-console.log('%cHello World', style);
+// let style = 'padding: 1rem; border: 2px solid crimson; border-radius: .5rem';
+// console.log('%cHello World', style);
+
+// What is syntax?
+//  Syntax is a set of rule that defines how the collection of charecters and symbols should be structured within a programming languuage
+
+// Stntax is a set of rules that defines how the collection of charecters and symbols should be structured within a programming language
+
+// What is statement
+// instructions that are executed bt the browser or node.js
---
+++ 🪙Chapter 2 Variables, Data Types, Methods, and More/04 Comments and Shortcuts/script.js
@@ -0,0 +1,43 @@
+// // Single line Comment
+// /*
+// Multi
+// Line
+// Comment
+// */
+// console.log('object');
+// // Single line Comment
+// /*
+// Multi
+// Line
+// Comment
+// */
+
+// console.log('object');
+// console.log('object');
+
+// console.log('object');
+// console.log('object');
+// console.log('object');
+// console.log('object');
+// console.log('object');
+// console.log('object');
+// console.log('object');
+// console.log('object');
+// console.log('object');
+// `
+// ~`;
+/*
+let camelCase;
+
+let PascalCase;
+
+let under_score;
+
+*/
+console.log('Test');
+console.log('Test');
+console.log('Test');
+console.log('Test');
+console.log('Test');
+console.log('Test');
+console.log('Test');
