0124dceedb42f5f452168c4ce73b17e6bb15953e
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-12 02:53:17 +0600

    !

--- 🎴Flashcard/JavaScript/24-03-10/JS-🔟-05 - setInterval and clearInterval Functions/script.js
+++ 🎴Flashcard/JavaScript/24-03-10/JS-🔟-05 - setInterval and clearInterval Functions/script.js
@@ -1,21 +1,21 @@
-// function currentTime() {
-//   console.log(Date.now());
+// // function currentTime() {
+// //   console.log(Date.now());
+// // }
+
+// // setInterval(currentTime, 5000);
+
+// document.querySelector('body').style.background = 'black';
+
+// let interval;
+
+// function colorChanger() {
+//   if (document.querySelector('body').style.background !== 'black') {
+//     document.querySelector('body').style.background = 'black';
+//     document.querySelector('body').style.color = 'white';
+//   } else {
+//     document.querySelector('body').style.background = 'white';
+//     document.querySelector('body').style.color = 'black';
+//   }
 // }
 
-// setInterval(currentTime, 5000);
-
-document.querySelector('body').style.background = 'black';
-
-let interval;
-
-function colorChanger() {
-  if (document.querySelector('body').style.background !== 'black') {
-    document.querySelector('body').style.background = 'black';
-    document.querySelector('body').style.color = 'white';
-  } else {
-    document.querySelector('body').style.background = 'white';
-    document.querySelector('body').style.color = 'black';
-  }
-}
-
-setInterval(colorChanger, 1000);
+// setInterval(colorChanger, 1000);
