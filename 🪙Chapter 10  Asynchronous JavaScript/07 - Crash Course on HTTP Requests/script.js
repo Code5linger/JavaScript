516140afa5bfbc1bfa8dadcfa48645de6df26782
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-13 01:51:31 +0600

    09 - AJAX and XHR Object

--- 🪙Chapter 10  Asynchronous JavaScript/07 - Crash Course on HTTP Requests/script.js
+++ 🪙Chapter 10  Asynchronous JavaScript/07 - Crash Course on HTTP Requests/script.js
@@ -1,4 +1,20 @@
 // 0114_Crash_Course_On_HTTP_Requests
 
 // HTTP Requests
-//
+
+// HTTP is a client/server protocol. Requests are initiated by the client (you) and the server will respond with a 'respose'. The response could be HTML, CSS, JS files, images and other resources as well as data formatted as JSON or XML
+
+// HTTP Methods
+// GET Request          - Fetch/retrive data from the server
+// POST Request         - Send data to the server
+// PUT & PATCH Request  - Update data on a server
+// DELETE Request       - Delete data from a server
+
+// HTTP Status Codes
+// 100 Range    - Continue
+// 200 Range    - Success
+// 201          - Created
+
+// 300 Range    - Redirects
+// 400 Range    - Client Error
+// 500 Range    - Server Error
