83e6e4e06579a8319b68801ec078e542afa8c3d8
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-12 22:59:20 +0600

    !

---
+++ 🎴Flashcard/JavaScript/24-03-11/JS-🔟-6️⃣-Callbacks/B/script.js
@@ -0,0 +1,44 @@
+// function toggle(event) {
+//   event.target.classList.toggle('danger');
+// }
+
+// document.querySelector('#btn').addEventListener('click', toggle);
+
+const posts = [
+  {
+    title: '5 Game-Changing GitHub Repositories for Developers',
+    body: 'In the massive world of programming and development, keeping up with the latest tools and resources can greatly boost your productivityBelow are five GitHub repositories that could change your workflow incredibly and broaden your knowledge base:',
+  },
+  {
+    title: 'Stash changes in a git repository with VS Code',
+    body: 'is a useful command that temporarily stores current changes in a Git repository without committing them, making it possible to return to them later.',
+  },
+];
+
+// function getPost() {
+//   setTimeout(() => {
+//     posts.forEach(function (content) {
+//       const div = document.createElement('div');
+//       div.innerHTML = `<strong>${content.title}</strong> - ${content.body}`;
+//       document.querySelector('.posts').appendChild(div);
+//     });
+//   }, 1000);
+// }
+
+// getPost();
+
+// function getpost() {
+//   posts.forEach(function (content) {
+//     const div = document.createElement('div');
+//     div.innerHTML = `<strong>${content.title}</strong> - ${content.body}`;
+//     document.querySelector('.posts').appendChild(div);
+//   });
+// }
+
+// getpost();
+
+// function toggleBtnBG(event) {
+//   event.target.classList.toggle('danger');
+// }
+
+// document.querySelector('#btn').addEventListener('click', toggleBtnBG);
