bff6fc54f357248bd3e10245a8e5e6583cc92497
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-14 23:22:17 +0600

    !

--- 🪙Chapter 10  Asynchronous JavaScript/15 - Promises Versus Callback Hell/script.js
+++ 🪙Chapter 10  Asynchronous JavaScript/15 - Promises Versus Callback Hell/script.js
@@ -20,7 +20,7 @@
 //     }, Math.floor(Math.random() * 3000) + 1000);
 //   });
 // }
-
+console.log();
 // // getData('./movies.json', (data) => {
 // //   console.log(data);
 // //   getData('./actors.json', (data) => {
@@ -46,3 +46,67 @@
 //   .catch((error) => {
 //     console.log(error);
 //   });
+
+console.log();
+
+// function getData(endpoint) {
+//   return new Promise((resolve, reject) => {
+//     let xhr = new XMLHttpRequest();
+
+//     xhr.open('GET', endpoint);
+
+//     if (this.readystate === 4) {
+//       if (this.status === 200) {
+//         setTimeout(() => {
+//           xhr.onreadystatechange = function () {
+//             resolve(JSON.parse(this.responseText));
+//           };
+//         }, 1000);
+//       } else {
+//         reject('Error');
+//       }
+//     }
+//     setTimeout(() => {
+//       xhr.send();
+//     }, Math.floor(Math.random() * 3000) + 1000);
+//   });
+// }
+
+// getData('./movies.json').then((movies) => {
+//   console.log(movies);
+// });
+
+// function getData(endpoint) {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', endpoint);
+
+//     xhr.onreadystatechange = function () {
+//       if (this.readyState === 4) {
+//         if (this.status === 200) {
+//           resolve(JSON.parse(this.responseText));
+//         } else {
+//           reject('T_T');
+//         }
+//       }
+//     };
+//     setTimeout(() => {
+//       xhr.send();
+//     }, Math.floor(Math.random() * 3000) + 1000);
+//   });
+// }
+
+// getData('./movies.json')
+//   .then((movies) => {
+//     console.log(movies);
+//     return getData('./directors.json');
+//   })
+//   .then((directors) => {
+//     console.log(directors);
+//     return getData('actors.json');
+//   })
+//   .then((actors) => {
+//     console.log(actors);
+//   });
+
+console.log();
