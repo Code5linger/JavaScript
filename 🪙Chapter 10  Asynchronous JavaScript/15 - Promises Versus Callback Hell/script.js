dc726d7272cd39699d505bf5dba7890550cab57c
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-14 22:29:11 +0600

    !

--- 🪙Chapter 10  Asynchronous JavaScript/15 - Promises Versus Callback Hell/script.js
+++ 🪙Chapter 10  Asynchronous JavaScript/15 - Promises Versus Callback Hell/script.js
@@ -1,36 +1,48 @@
-// 15 - Promises Versus Callback Hell
-function getData(endpoint) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
+// // 15 - Promises Versus Callback Hell
+// function getData(endpoint) {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', endpoint);
+//     xhr.open('GET', endpoint);
 
-    xhr.onreadystatechange = function () {
-      if (this.readyState === 4) {
-        if (this.status === 200) {
-          resolve(JSON.parse(this.responseText));
-        } else {
-          reject('Something ...');
-        }
-      }
-    };
+//     xhr.onreadystatechange = function () {
+//       if (this.readyState === 4) {
+//         if (this.status === 200) {
+//           resolve(JSON.parse(this.responseText));
+//         } else {
+//           reject('Something ...');
+//         }
+//       }
+//     };
 
-    setTimeout(() => {
-      xhr.send();
-    }, Math.floor(Math.random() * 3000) + 1000);
-  });
-}
-
-// getData('./movies.json', (data) => {
-//   console.log(data);
-//   getData('./actors.json', (data) => {
-//     console.log(data);
-//     getData('./directos.json', (data) => {
-//       console.log(data);
-//     });
+//     setTimeout(() => {
+//       xhr.send();
+//     }, Math.floor(Math.random() * 3000) + 1000);
 //   });
-// });
+// }
 
-getData('./movies.json').then((movies) => {
-  console.log(movies);
-});
+// // getData('./movies.json', (data) => {
+// //   console.log(data);
+// //   getData('./actors.json', (data) => {
+// //     console.log(data);
+// //     getData('./directos.json', (data) => {
+// //       console.log(data);
+// //     });
+// //   });
+// // });
+
+// getData('./movies.json')
+//   .then((movies) => {
+//     console.log(movies);
+//     return getData('./actors.json');
+//   })
+//   .then((actors) => {
+//     console.log(actors);
+//     return getData('./directorsS.json');
+//   })
+//   .then((directors) => {
+//     console.log(directors);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
