4ea0be1dbf1a85c764f086d9ba439f113b45229f
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-14 20:45:30 +0600

    !

--- 🪙Chapter 10  Asynchronous JavaScript/14 - Promise Chaining/script.js
+++ 🪙Chapter 10  Asynchronous JavaScript/14 - Promise Chaining/script.js
@@ -1,4 +1,95 @@
-//  14 - Promise Chaining
+// //  14 - Promise Chaining
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     let error = false;
+
+//     if (!error) {
+//       resolve({ name: 'Elrick', age: 69 });
+//     } else {
+//       reject('Error');
+//     }
+//   }, 1000);
+// });
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     let error = true;
+
+//     if (!error) {
+//       resolve({ name: 'Elrick', age: 69 });
+//     } else {
+//       reject('Error🚩');
+//     }
+//   }, 1000);
+// });
+
+// promise
+//   .then((user) => {
+//     console.log(user);
+//     return user.name;
+//   })
+//   .then((name) => {
+//     console.log(name);
+//     return name.length;
+//   })
+//   .then((nameLength) => {
+//     console.log(nameLength);
+//     // return name.length;
+//   })
+//   .catch((error) => console.log(error))
+//   .then(() => {
+//     console.log('Error 1');
+//     return 69;
+//   })
+//   .then((x) => console.log(x))
+//   .finally(() => console.log('✒️fin'));
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     let error = true;
+
+//     if (!error) {
+//       resolve({ name: 'Elrick', age: 69 });
+//     } else {
+//       reject('Error🚩');
+//     }
+//   }, 1000);
+// });
+
+// promise
+//   .then((user) => {
+//     console.log(user);
+//     return user.name;
+//   })
+//   .then((name) => {
+//     console.log(name);
+//     return name.length;
+//   })
+//   .then((nameLength) => {
+//     console.log(nameLength);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//     return 69;
+//   })
+//   .then(() => {
+//     console.log(`error part 2`);
+//     return 69;
+//   })
+//   .then((x) => {
+//     console.log(x);
+//     return 420;
+//   })
+//   .then((y) => {
+//     console.log(y);
+//   })
+//   .finally(() => {
+//     console.log('I am inhabitable 🫰');
+//   })
+//   .then(() => {
+//     console.log('& I am iron man');
+//   });
+
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
     let error = false;
@@ -10,3 +101,33 @@ const promise = new Promise((resolve, reject) => {
     }
   }, 1000);
 });
+
+promise
+  .then((user) => {
+    console.log(user);
+    return user.name;
+  })
+  .then((name) => {
+    console.log(name);
+    return name.length;
+  })
+  .then((nameLength) => {
+    console.log(nameLength);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .then(() => {
+    console.log('Error the ERROR');
+    return 69;
+  })
+  .then((x) => {
+    console.log(x);
+    return 420;
+  })
+  .then((y) => {
+    console.log(y);
+  })
+  .finally(() => {
+    console.log('✒️fin');
+  });
