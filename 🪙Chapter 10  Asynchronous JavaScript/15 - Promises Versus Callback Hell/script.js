05d3bab8f36c9e0ebd0a9e4a665d81744bdd5902
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-15 10:28:45 +0600

    !

--- 🪙Chapter 10  Asynchronous JavaScript/15 - Promises Versus Callback Hell/script.js
+++ 🪙Chapter 10  Asynchronous JavaScript/15 - Promises Versus Callback Hell/script.js
@@ -3,6 +3,10 @@
 //   return new Promise((resolve, reject) => {
 //     const xhr = new XMLHttpRequest();
 
+// const { error } = require("console");
+
+// const { error } = require("console");
+
 //     xhr.open('GET', endpoint);
 
 //     xhr.onreadystatechange = function () {
@@ -174,28 +178,96 @@ console.log();
 
 console.log();
 
+// function getData(endpoint) {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+
+//     xhr.open('GET', endpoint);
+
+//     xhr.onreadystatechange = function () {
+//       if (this.readyState === 4) {
+//         if (this.status === 200) {
+//           resolve(JSON.parse(this.responseText));
+//         } else {
+//           reject('Error Has Occared!');
+//         }
+//       }
+//     };
+
+//     setTimeout(() => {
+//       xhr.send();
+//     }, 1000);
+//   });
+// }
+
+// getData('./movies.json').then((movies) => {
+//   console.log(movies);
+// });
+
 function getData(endpoint) {
   return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-
-    xhr.open('GET', endpoint);
-
-    xhr.onreadystatechange = function () {
-      if (this.readyState === 4) {
-        if (this.status === 200) {
-          resolve(JSON.parse(this.responseText));
-        } else {
-          reject('Error Has Occared!');
-        }
-      }
-    };
-
     setTimeout(() => {
-      xhr.send();
-    }, 1000);
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', endpoint);
+
+      xhr.onreadystatechange = function () {
+        if (this.readyState === 4) {
+          if (this.status === 200) {
+            resolve(JSON.parse(this.responseText));
+          } else {
+            reject('Error');
+          }
+        }
+      };
+
+      setTimeout(() => {
+        xhr.send();
+      }, Math.floor(Math.random() * 3000) + 1000);
+    });
   });
 }
 
-getData('./movies.json').then((movies) => {
-  console.log(movies);
+// getData('./movies.json')
+//   .then((movies) => {
+//     console.log(movies);
+//     return getData('./actors.json');
+//   })
+//   .then((actors) => {
+//     console.log(actors);
+//     return getData('./directors.json');
+//   })
+//   .then((directors) => {
+//     console.log(directors);
+//   });
+console.log();
+// const moviesPromise = getData('./movies.json');
+// const actorsPromise = getData('./actors.json');
+// const directorsPromise = getData('./directorsX.json');
+
+// moviesPromise.then((movies) => console.log(movies));
+// actorsPromise.then((actors) => console.log(actors));
+// directorsPromise.then((directors) => console.log(directors));
+
+// Promise.all([moviesPromise, actorsPromise, directorsPromise])
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => console.log(error));
+
+const moviesPromise = getData('./movies.json');
+const actorsPromise = getData('./actors.json');
+const directorsPromise = getData('./directors.json');
+
+// moviesPromise.then((movie) => console.log(movie));
+// actorsPromise.then((actors) => console.log(actors));
+// directorsPromise.then((directors) => console.log(directors));
+
+const testPromise = new Promise((resolve, reject) => {
+  resolve('Hello  World');
 });
+
+Promise.all([moviesPromise, actorsPromise, directorsPromise, testPromise])
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.log(error));
