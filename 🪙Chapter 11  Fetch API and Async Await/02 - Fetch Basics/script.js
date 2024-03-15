1a85fc46f113f053f3cfba48f03059b47ea5de59
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-16 02:37:04 +0600

    !

---
+++ 🪙Chapter 11  Fetch API and Async Await/02 - Fetch Basics/script.js
@@ -0,0 +1,14 @@
+console.log('Test');
+
+// fetch('./movies.json')
+//   .then((response) => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+fetch('./movies.json')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
