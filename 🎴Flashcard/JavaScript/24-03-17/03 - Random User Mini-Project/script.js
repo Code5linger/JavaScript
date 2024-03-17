8edfae1fc3cd95ed02285cf3769f673448aca87f
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-17 16:03:29 +0600

    !

---
+++ 🎴Flashcard/JavaScript/24-03-17/03 - Random User Mini-Project/script.js
@@ -0,0 +1,40 @@
+function fetchUser() {
+  fetch('https://randomuser.me/api/')
+    .then((res) => res.json())
+    .then((data) => displayUser(data.results[0]));
+}
+function displayUser(user) {
+  let element = document.querySelector('main');
+  let bgSelector = document.body.style;
+  let btnColor = document.querySelector('button');
+
+  console.log(user);
+
+  if (user.gender === 'female') {
+    bgSelector.background = `#ff77ff`;
+    btnColor.color = 'black';
+  } else {
+    bgSelector.background = `#00308F`;
+    bgSelector.color = 'white';
+  }
+
+  element.innerHTML = `
+        <img
+          class="imageContent"
+          src="${user.picture.large}"
+          alt=""
+          srcset=""
+        />
+        <div class="textContent">
+          <h3>Name: ${user.name.first} ${user.name.last}</h3>
+          <h3>Email: ${user.email}</h3>
+          <h3>Phone: ${user.phone}</h3>
+          <h3>Location: ${user.location.city}, ${user.location.counrty}</h3>
+          <h3>Age: ${user.dob.age}</h3>
+        </div>
+    `;
+}
+
+document.querySelector('button').addEventListener('click', fetchUser);
+
+addEventListener('DOMContentLoaded', fetchUser);
