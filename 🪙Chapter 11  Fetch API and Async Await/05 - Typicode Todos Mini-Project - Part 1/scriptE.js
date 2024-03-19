2e31e68b6ac3d565f252155be8497290b74184ce
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-19 10:46:52 +0600

    05 - Typicode Todos Mini-Project - Part 1

---
+++ 🪙Chapter 11  Fetch API and Async Await/05 - Typicode Todos Mini-Project - Part 1/scriptE.js
@@ -0,0 +1,45 @@
+const apiURl = 'https://jsonplaceholder.typicode.com/todos';
+
+const fetchAPI = () => {
+  fetch(apiURl + '?_limit=10')
+    .then((response) => response.json())
+    .then((todo) => todo.forEach((todo) => addToDosToDom(todo)));
+};
+
+const addToDosToDom = (todo) => {
+  const div = document.createElement('div');
+  div.appendChild(document.createTextNode(todo.title));
+
+  document.querySelector('#todo-list').appendChild(div);
+
+  if (todo.completed === true) {
+    div.classList.add('done');
+  }
+
+  div.setAttribute('data-id', todo.id);
+};
+
+const addNewTodos = (event) => {
+  event.preventDefault();
+  console.log(event.target.firstElementChild.value);
+
+  fetch(apiURl, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: event.target.firstElementChild.value,
+      completed: false,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => addToDosToDom(data));
+};
+
+const initiate = () => {
+  document.addEventListener('DOMContentLoaded', fetchAPI);
+  document.querySelector('#todo-form').addEventListener('submit', addNewTodos);
+};
+
+initiate();
---
+++ 🪙Chapter 11  Fetch API and Async Await/05 - Typicode Todos Mini-Project - Part 1/scriptF.js
@@ -0,0 +1,47 @@
+const apiURL = 'https://jsonplaceholder.typicode.com/todos';
+
+const fetchAPI = () => {
+  fetch(apiURL + '?_limit=10')
+    .then((res) => res.json())
+    .then((todos) => todos.forEach((todos) => addToDosToDOM(todos)));
+};
+
+const addToDosToDOM = (todos) => {
+  const div = document.createElement('div');
+  div.appendChild(document.createTextNode(todos.title));
+
+  if (todos.completed === true) {
+    div.classList.add('done');
+  }
+
+  div.setAttribute('data-id', todos.id);
+
+  document.querySelector('#todo-list').appendChild(div);
+};
+
+const createNewToDos = (event) => {
+  event.preventDefault();
+  console.log(event.target.firstElementChild.value);
+
+  fetch(apiURL, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: event.target.firstElementChild.value,
+      completed: false,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => addToDosToDOM(data));
+};
+
+const initialze = () => {
+  document.addEventListener('DOMContentLoaded', fetchAPI);
+  document
+    .querySelector('#todo-form')
+    .addEventListener('submit', createNewToDos);
+};
+
+initialze();
