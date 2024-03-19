ede008343fff5af29071b48844e9edb3e313a02a
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-20 00:31:02 +0600

    !

--- 🪙Chapter 11  Fetch API and Async Await/05 - Typicode Todos Mini-Project - Part 1/scriptG.js
+++ 🪙Chapter 11  Fetch API and Async Await/05 - Typicode Todos Mini-Project - Part 1/scriptG.js
@@ -8,6 +8,7 @@ const fetchAPI = () => {
 
 const addTodosToDom = (todo) => {
   const div = document.createElement('div');
+  div.classList.add('todo');
   div.appendChild(document.createTextNode(todo.title));
   document.querySelector('#todo-list').appendChild(div);
 
@@ -24,6 +25,7 @@ const addNewTodos = (event) => {
 
   fetch(apiURL, {
     method: 'POST',
+
     headers: {
       'content-type': 'application/json',
     },
@@ -36,9 +38,35 @@ const addNewTodos = (event) => {
     .then((data) => addTodosToDom(data));
 };
 
+const toggleCompleted = (event) => {
+  if (event.target.classList.contains('todo')) {
+    event.target.classList.toggle('done');
+
+    updateTodo(
+      event.target.dataset.id,
+      event.target.classList.contains('done')
+    );
+  }
+};
+
+const updateTodo = (id, completed) => {
+  fetch(`${apiURL}/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ completed }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
+
 const initiat = () => {
   document.addEventListener('DOMContentLoaded', fetchAPI);
   document.querySelector('#todo-form').addEventListener('submit', addNewTodos);
+  document
+    .querySelector('#todo-list')
+    .addEventListener('click', toggleCompleted);
 };
 
 initiat();
