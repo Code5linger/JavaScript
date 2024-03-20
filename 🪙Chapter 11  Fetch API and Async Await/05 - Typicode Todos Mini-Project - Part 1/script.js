87e3c466ecee821b635d68aa6054d6bc035b16b0
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-21 03:48:26 +0600

    !

--- 🪙Chapter 11  Fetch API and Async Await/05 - Typicode Todos Mini-Project - Part 1/script.js
+++ 🪙Chapter 11  Fetch API and Async Await/05 - Typicode Todos Mini-Project - Part 1/script.js
@@ -7,9 +7,11 @@ const fetchAPI = () => {
     .then((todo) => todo.forEach((todo) => addTodosToDOM(todo)));
 };
 
-const addTodosToDOM = (todo, completed) => {
+const addTodosToDOM = (todo) => {
   const div = document.createElement('div');
+  div.classList.add('todo');
   div.appendChild(document.createTextNode(todo.title));
+  div.setAttribute('data-id', todo.id);
 
   if (todo.completed === true) {
     //⛑️
@@ -20,9 +22,69 @@ const addTodosToDOM = (todo, completed) => {
 };
 
 // Adding new Todo
+const addingNewToDO = (event) => {
+  event.preventDefault();
+
+  fetch(apiURL, {
+    //⛑️
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      title: event.target.firstElementChild.value,
+      completed: false,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => addTodosToDOM(data));
+};
 
 // Updating Todo
+const toggleComplete = (event) => {
+  if (event.target.classList.contains('todo')) {
+    event.target.classList.toggle('done'); //⛑️
+
+    updateToDo(
+      event.target.dataset.id,
+      event.target.classList.contains('done')
+    );
+  }
+};
+
+const updateToDo = (id, completed) => {
+  fetch(`${apiURL}/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      completed,
+    }),
+  });
+};
 
 // Deleting TODO
+const deleteTODO = (event) => {
+  if (event.target.classList.contains('todo')) {
+    const id = event.target.dataset.id;
+    fetch(`${apiURL}/${id}`)
+      .then((res) => res.json())
+      .then(() => event.target.remove());
+  }
+};
 
-fetchAPI();
+// Event Manager
+const initialze = () => {
+  document.addEventListener('DOMContentLoaded', fetchAPI);
+
+  document
+    .querySelector('#todo-form')
+    .addEventListener('submit', addingNewToDO);
+
+  document
+    .querySelector('#todo-list')
+    .addEventListener('click', toggleComplete);
+
+  document.querySelector('#todo-list').addEventListener('dblclick', deleteTODO);
+};
+
+initialze();
