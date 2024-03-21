fd4c1d7231559d2b741704edc054dc64c7205f69
Code5linger <sakib.ahmed.1708@gmail.com>
2024-03-22 03:06:44 +0600

    !

---
+++ 🪙Chapter 11  Fetch API and Async Await/08 - Async and Await/script.js
@@ -0,0 +1,23 @@
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({ name: 'Ash', age: 20 });
+  }, 1000);
+});
+
+// promise.then((data) => console.log(data));
+
+async function getPromise() {
+  const response = await promise;
+  console.log(response);
+}
+
+// getPromise();
+
+async function getUsers() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  console.log(data);
+}
+
+getUsers();
---
+++ 🪙Chapter 11  Fetch API and Async Await/09 - Try...Catch Statements/script.js
@@ -0,0 +1,41 @@
+// try {
+//   console.log(x);
+// } catch (error) {
+//   console.log('Error ' + error);
+// }
+
+// function double(number) {
+//   if (isNaN(number)) {
+//     throw new Error(number + ' is not a number');
+//   }
+//   return number * 2;
+// }
+
+// try {
+//   const y = double('hello');
+//   console.log(y);
+// } catch (error) {
+//   console.log(error);
+// }
+
+console.log();
+
+// try {
+//   console.log(x);
+// } catch (error) {
+//   console.log('❌ Error: ' + error);
+// }
+
+function double(number) {
+  if (isNaN(number)) {
+    throw new Error(number + ' is not a number');
+  }
+  return number * 2;
+}
+
+try {
+  const y = double('H');
+  console.log(y);
+} catch (error) {
+  console.log(error);
+}
